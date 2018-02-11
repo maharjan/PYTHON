e1e2df9eaaa21cd3f081ef84a638ea06ec6b44b7
Prajin Maharjan <prajin@esewa.com.np>
2018-02-11 17:35:20 +0545

    list and string

---
+++ basic/lists.py
@@ -0,0 +1,19 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+mylist.append(5)
+
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+print(mylist[4])
+print("\n")
+
+for x in mylist:
+    print(x)
+# print("\n")
+mylist1 = [1,2,3,"Test"]
+print(mylist1[3])
